e2592e517444ccae09df565a2a11902c65341e9a
avis93 <33150483+avis93@users.noreply.github.com>
2018-03-21 19:47:32 +0100

    grafika

---
+++ agit/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(463, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(78, 74);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(266, 248);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(78, 346);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(266, 209);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(585, 116);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(223, 205);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 80;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 567);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ agit/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int status = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private unsafe void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+
+        }
+
+        private unsafe void timer1_Tick(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(128, 128, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+            Bitmap bmpA = new Bitmap(128, 128, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+            Bitmap bmpB = new Bitmap(128, 128, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+
+            Graphics g = Graphics.FromImage(bmpA);
+            g.DrawString("A", new Font("Arial", 100), Brushes.Red, new Point(0, 0));
+            this.pictureBox2.Image = bmpA;
+            g = Graphics.FromImage(bmpB);
+            g.DrawString("B", new Font("Arial", 100), Brushes.Blue, new Point(0, 0));
+            this.pictureBox3.Image = bmpB;
+            g.Flush();
+
+
+
+
+
+
+            BitmapData bm = bmp.LockBits(new Rectangle(Point.Empty, bmp.Size), ImageLockMode.ReadWrite, bmp.PixelFormat);
+            UInt32* ptr = (UInt32*)bm.Scan0.ToPointer();
+
+            BitmapData bmA = bmpA.LockBits(new Rectangle(Point.Empty, bmpA.Size), ImageLockMode.ReadWrite, bmpA.PixelFormat);
+            UInt32* ptrA = (UInt32*)bmA.Scan0.ToPointer();
+
+            BitmapData bmB = bmpB.LockBits(new Rectangle(Point.Empty, bmpB.Size), ImageLockMode.ReadWrite, bmpB.PixelFormat);
+            UInt32* ptrB = (UInt32*)bmB.Scan0.ToPointer();
+            //przypisanie wskaźnika 
+            //Scan0 - 1 bajt pierwszego piksela 
+
+            switch(status) {
+
+                case 0 :
+
+                    for(int i = 0; i < 128 * 128; i++)
+                    {
+                        *ptr = *ptrA;
+                        ptr++;
+                        ptrA++;
+                    }
+                    break;
+                case 2:
+
+                    for(int i =0; i< 128*128; i++)
+                    {
+                        if(*ptrB == 0xFF0000FF)
+                        {
+                            *ptr = 0x00000000;
+                                 
+                        } else
+                        {
+                            *ptr = *ptrA;
+                        }
+                        ptr++;
+                        ptrA++;
+                        ptrB++;
+                    }
+
+                    break;
+                case 3:
+
+                    for (int i = 0; i < 128 * 128; i++)
+                    {
+                        *ptr = *ptrB;
+                        ptr++;
+                        ptrB++;
+                    }
+                        break;
+
+                case 4:
+                    for (int i = 0; i < 128 * 128; i++)
+                    {
+                        if (*ptrA == 0xFFFF0000)
+                        {
+                            *ptr = 0x00000000;
+
+                        }
+                        else
+                        {
+                            *ptr = *ptrB;
+                        }
+                        ptr++;
+                        ptrA++;
+                        ptrB++;
+                    }
+                    break;
+                case 5:
+                    
+
+                    for (int i = 0; i < 128 * 128; i++)
+                    {
+                        if (*ptrA == *ptrB)
+                        {
+                            *ptr = 0x00000000;
+                        }
+                        else if ((*ptrA == 0xFFFF0000) && (*ptrB == 0xFF0000FF))
+                        {
+                            *ptr = (*ptrA + *ptrB) / 2;
+                        }
+                        else if ((*ptrA == 0xFFFF0000) && (*ptrB == 0x00000000))
+                        {
+                            *ptr = *ptrA;
+                        }
+                        else
+                        {
+                            *ptr = *ptrB;
+                        }
+
+                        ptr++;
+                        ptrB++;
+                        ptrA++;
+
+
+                    }
+                    break;
+
+            }
+
+
+
+
+
+
+
+            bmp.UnlockBits(bm);
+            bmpA.UnlockBits(bmA);
+            bmpB.UnlockBits(bmB);
+
+            this.pictureBox1.Image = bmp;
+
+            status++;
+
+            if (status == 6) status = 0;
+        }
+    }
+}
